86c837f9b3d1ca695a52d4c192e52f6c01a7e70c
prabhu <prabhu_tc@yahoo.com>
2023-01-28 19:50:33 +0530

    [Updating files]

--- ResumePage.js
+++ ResumePage.js
@@ -2,8 +2,7 @@
 let jsonResumeObj;
 const arr_Applied_for = [];
 const arr_Applied_for_filtered = [];
-let resumecode=" ";
-
+let resumecode = " ";
 
 const buttonPrevious = document.querySelector(".previousButton");
 const searchbox = document.querySelector(".searchTextBox");
@@ -13,186 +12,112 @@ buttonPrevious.addEventListener("click", buttonPreviousClicked);
 buttonNext.addEventListener("click", buttonNextClicked);
 searchbox.addEventListener("change", searchString);
 
-
 let can_Index = 0;
 
-/*
+//not using direct data file due to CORS error in Chrome browser
+//let jsonUrl = "./Data.json";
 
-let jsonUrl = "./Data.json";
-//For testing in Chrome Browser
+//Data.json file is hosted in below path and hence reading from this url to avoid CORS error
 let jsonUrl = "https://api.npoint.io/aeddb50e5de8ef107bf0";
 
-$(document).ready(function (e) {
-    $.getJSON(jsonUrl, function (data) {
+//using jquery to load json data
+// $(document).ready(function (e) {
+//     $.getJSON(jsonUrl, function (data) {
+//         jsonResumeObj = data;
+//         if (jsonResumeObj.resume.length > 0) {
+//             for (let x in jsonResumeObj.resume) {
+//                 arr_Applied_for[x] = jsonResumeObj.resume[x].basics.AppliedFor;
+//             }
+//             can_Index = -1;
+//             buttonNextClicked();
+//         } else {
+//             buttonNext.style.visibility = 'hidden';
+//             Alert(" No resume avaialble");
+//         }
+//     });
+// });
 
-        console.log(data);
 
-        jsonResumeObj = data;
-
-        console.log(jsonResumeObj);
-
-        if (jsonResumeObj.resume.length > 0) {
-            displayResume(0);
-            for (let x in jsonResumeObj.resume) {
-                arr_Applied_for[x] = jsonResumeObj.resume[x].basics.AppliedFor;
-            }
-        }
-    });
-});
-*/
-
-//getText("http://localhost:8080/TestWeb/Data.json");
-let jsonUrl = "https://api.npoint.io/aeddb50e5de8ef107bf0";
 getText(jsonUrl);
 
 async function getText(file) {
-  let x = await fetch(file);
-  let y = await x.text();
-  jsonResumeObj = JSON.parse(y);
-  if(jsonResumeObj.resume.length>0){
-     for (let x in jsonResumeObj.resume) {
-       arr_Applied_for[x]=jsonResumeObj.resume[x].basics.AppliedFor;
-     }
-     can_Index = -1;
-     buttonNextClicked();
-  } else{
-    buttonNext.style.visibility = 'hidden';
-    Alert (" No resume avaialble");
-  }
-} 
+    let x = await fetch(file);
+    let y = await x.text();
+    jsonResumeObj = JSON.parse(y);
+    if (jsonResumeObj.resume.length > 0) {
+        for (let x in jsonResumeObj.resume) {
+            arr_Applied_for[x] = jsonResumeObj.resume[x].basics.AppliedFor;
+        }
+        can_Index = -1;
+        buttonNextClicked();
+    } else {
+        buttonNext.style.visibility = 'hidden';
+        Alert(" No resume avaialble");
+    }
+}
 
 
 function buttonNextClicked() {
-  /*  if (searchbox.value.length > 0) {
-        if (can_Index < (arr_Applied_for_filtered.length - 1)) {
-            can_Index++;
-            displayResume(arr_Applied_for_filtered[can_Index]);
-            buttonPrevious.style.visibility = 'visible';
-            if(can_Index ==0)
-            buttonPrevious.style.visibility = 'hidden';
-            buttonNext.style.visibility = 'visible';
-            if(can_Index == (arr_Applied_for_filtered.length - 1))
-            buttonNext.style.visibility = 'hidden';
-        }
-    } else {
-        if (can_Index < (jsonResumeObj.resume.length - 1)) {
-            can_Index++;
-            displayResume(can_Index);
-            buttonPrevious.style.visibility = 'visible';
-            if(can_Index ==0)
-            buttonPrevious.style.visibility = 'hidden';
-            buttonNext.style.visibility = 'visible';
-            if(can_Index == (jsonResumeObj.resume.length - 1))
-            buttonNext.style.visibility = 'hidden';
-        }
-    }*/
-    let max_Index=0;
-    let dis_Index=0;
+    let max_Index = 0;
+    let dis_Index = 0;
     if (searchbox.value.length > 0) {
-        max_Index=arr_Applied_for_filtered.length - 1;
-        dis_Index=arr_Applied_for_filtered[can_Index +1];
-    } else{
-        max_Index=jsonResumeObj.resume.length - 1;
-        dis_Index=can_Index +1;
+        max_Index = arr_Applied_for_filtered.length - 1;
+        dis_Index = arr_Applied_for_filtered[can_Index + 1];
+    } else {
+        max_Index = jsonResumeObj.resume.length - 1;
+        dis_Index = can_Index + 1;
     }
     if (can_Index < max_Index) {
         can_Index++;
         displayResume(dis_Index);
-        buttonPrevious.style.visibility = (can_Index ==0)?'hidden':'visible';
-        buttonNext.style.visibility =(can_Index ==  max_Index)?'hidden':'visible';
+        buttonPrevious.style.visibility = (can_Index == 0) ? 'hidden' : 'visible';
+        buttonNext.style.visibility = (can_Index == max_Index) ? 'hidden' : 'visible';
     }
 }
 
 function buttonPreviousClicked() {
-    let max_Index=0;
-    let dis_Index=0;
+    let max_Index = 0;
+    let dis_Index = 0;
     if (searchbox.value.length > 0) {
-        max_Index=arr_Applied_for_filtered.length - 1;
-        dis_Index=  arr_Applied_for_filtered[can_Index -1]
-    }else{
-        max_Index=jsonResumeObj.resume.length - 1;
-        dis_Index=  can_Index-1;
+        max_Index = arr_Applied_for_filtered.length - 1;
+        dis_Index = arr_Applied_for_filtered[can_Index - 1]
+    } else {
+        max_Index = jsonResumeObj.resume.length - 1;
+        dis_Index = can_Index - 1;
     }
     if (can_Index > 0) {
         can_Index--;
         displayResume(dis_Index);
-        buttonPrevious.style.visibility = (can_Index ==0)?'hidden':'visible';
-        buttonNext.style.visibility =(can_Index ==  max_Index)?'hidden':'visible';
+        buttonPrevious.style.visibility = (can_Index == 0) ? 'hidden' : 'visible';
+        buttonNext.style.visibility = (can_Index == max_Index) ? 'hidden' : 'visible';
     }
-
-/*
-    if (searchbox.value.length > 0) {
-        if (can_Index > 0) {
-            can_Index--;
-            buttonNext.style.visibility = 'visible';
-            displayResume(arr_Applied_for_filtered[can_Index]);
-            if(can_Index ==0)
-            buttonPrevious.style.visibility = 'hidden';
-        }
-    } else {
-        if (can_Index > 0) {
-            can_Index--;
-            buttonNext.style.visibility = 'visible';
-            displayResume(can_Index);
-            if(can_Index ==0)
-            buttonPrevious.style.visibility = 'hidden';
-        }
-    }
-    */
 }
 
 function searchString() {
     arr_Applied_for_filtered.length = 0;
-    //alert(searchbox.value.length);
     for (let x in arr_Applied_for) {
-        //alert(arr_Applied_for[x]);
         if (arr_Applied_for[x].includes(searchbox.value)) {
             arr_Applied_for_filtered.push(x);
         }
     }
-    //alert(arr_Applied_for_filtered.length);
-    if(arr_Applied_for_filtered.length>0){
-        if( resumecode.length >5 ){
-            document.getElementById("resumeSection").innerHTML =resumecode;
-            resumecode=" ";
+    if (arr_Applied_for_filtered.length > 0) {
+        if (resumecode.length > 5) {
+            document.getElementById("resumeSection").innerHTML = resumecode;
+            resumecode = " ";
         }
         can_Index = -1;
         buttonNextClicked();
-    }else{
+    } else {
         buttonNext.style.visibility = 'hidden';
         buttonPrevious.style.visibility = 'hidden';
-        //alert("No Resume available");
-        resumecode=document.getElementById("resumeSection").innerHTML;
-        document.getElementById("resumeSection").innerHTML ="<div> <img src='Images/NoResume.jpg' alt='No Results'></div>"
+        resumecode = document.getElementById("resumeSection").innerHTML;
+        document.getElementById("resumeSection").innerHTML = "<div> <img src='Images/NoResume.jpg' alt='No Results'></div>"
     }
 }
 
 
 
 function displayResume(i) {
-    // const can_id = jsonResumeObj.resume[i].id;
-    // const can_image = jsonResumeObj.resume[i].basics.image;
-    // const can_address = jsonResumeObj.resume[i].basics.location.address;
-    // const can_postalCode = jsonResumeObj.resume[i].basics.location.postalCode;
-    // const can_city = jsonResumeObj.resume[i].basics.location.city;
-    // const can_state = jsonResumeObj.resume[i].basics.location.state;
-    // const can_skills_name = jsonResumeObj.resume[i].skills.name;
-    // const can_skills_level = jsonResumeObj.resume[i].skills.level;
-
-   /* if (i === 0) {
-        buttonPrevious.style.visibility = 'hidden';
-    } else {
-        buttonPrevious.style.visibility = 'visible';
-    }
-
-    let lastIndex = jsonResumeObj.resume.length - 1;
-    if (i === lastIndex) {
-        buttonNext.style.visibility = 'hidden';
-    } else {
-        buttonNext.style.visibility = 'visible';
-    }
-    */
-
     const userNameTextValue = jsonResumeObj.resume[i].basics.name;
     document.getElementById("userName").innerHTML = userNameTextValue;
 
